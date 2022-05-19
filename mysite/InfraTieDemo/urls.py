b48e71a4b1eec093dcd999119a3795ca0b73fdb2
paulbernius <100249266+paulbernius@users.noreply.github.com>
2022-05-18 19:32:20 -0500

    Added Nav Bar

--- mysite/InfraTieDemo/urls.py
+++ mysite/InfraTieDemo/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('<int:InspectionID>/detail/', views.detail, name='detail'),
+    path('', views.home, name='home'),
+
 ]
